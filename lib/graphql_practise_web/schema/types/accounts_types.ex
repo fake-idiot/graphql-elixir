ee279f87fd1fdc4e9cb8ff5a994985c38207615c
Muhammad Hamza <hamzarakha786@gmail.com>
2023-10-28 19:53:53 +0500

    added accounts_types.ex file for user

---
+++ lib/graphql_practise_web/schema/types/accounts_types.ex
@@ -0,0 +1,18 @@
+defmodule GraphqlPractiseWeb.Schema.AccountsTypes do
+  use Absinthe.Schema.Notation
+
+  object(:user) do
+    field :id, :id
+    field :email, :string
+    field :password, :string
+    field :hashed_password, :string
+    field :confirmed_at, :string
+    field :inserted_at, :string
+    field :updated_at, :string
+  end
+
+  input_object :reister_user do
+    field :email, :string
+    field :password, :string
+  end
+end
